a5d0ff8fb15c78ff3ce5d161482102ddaed5a049
zhaozhilong <zhaozhilong@unitedstack.com>
2016-11-18 17:32:14 +0800

    Delete old unit test
    Gringotts version 3 should not use the unit test of version 2.

--- gringotts/tests/middleware/test_middleware.py
+++
@@ -1,8 +0,0 @@
-
-from gringotts.tests import core as tests
-
-
-class CommonMiddlewareTestCase(tests.MiddlewareTestCase):
-
-    def setUp(self):
-        super(CommonMiddlewareTestCase, self).setUp()
